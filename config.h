164bde6aef4b70a062a7d343dd342bc8ffba510d
Gustavo Meireles <gugatpm@gmail.com>
2023-08-17 00:55:39 +0100

    stuff

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 10;
+static const unsigned int gappx     = 0;
 static const unsigned int snap      = 16;       /* snap pixel */
 static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
